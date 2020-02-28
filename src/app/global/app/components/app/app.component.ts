e96ae2a3e9feb1602f6b9e57d70f6b1c3d033b7e
Daniel Richter <daniel.richter@teambank.de>
2020-02-28 16:22:17 +0100

    feat: make darkmode toggleable
    Signed-off-by: Daniel Richter <daniel.richter@teambank.de>
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- src/app/global/app/components/app/app.component.ts
+++ src/app/global/app/components/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {AuthenticationService} from "src/app/features/user/services/authentication.service";
+import {Overlay, OverlayContainer} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,28 @@ import {AuthenticationService} from "src/app/features/user/services/authenticati
 })
 export class AppComponent implements OnInit {
   loggedIn$: Observable<boolean> = this.authService.isLoggedIn();
+  @HostBinding('class') theme: string;
 
   constructor(
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private overlayContainer: OverlayContainer,
   ) {
   }
 
   ngOnInit(): void {
+    this.theme = 'theme-dark';
+    this.overlayContainer.getContainerElement().classList.add(this.theme)
   }
 
-  logout(){
+  logout() {
     this.authService.logout().subscribe();
+  }
+
+  toggleDarkMode() {
+    if (this.theme === 'theme-dark') {
+      this.theme='';
+    }else{
+      this.theme= 'theme-dark';
+    }
   }
 }
