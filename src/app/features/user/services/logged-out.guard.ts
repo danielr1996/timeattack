1a04df95e9067066c6f753528bb824d3198f0bb5
Daniel Richter <danielrichter@posteo.de>
2020-03-09 23:18:13 +0100

    refactor: Reviewanmerkungen
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- src/app/features/user/services/logged-out.guard.ts
+++ src/app/features/user/services/logged-out.guard.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
+import {map, tap} from "rxjs/operators";
+import {AuthenticationService} from "src/app/features/user/services/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggedOutGuard implements CanActivate {
+  constructor(private authService: AuthenticationService, private router: Router) {
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    console.log('Checking if user is logged out')
+    return this.authService.isLoggedIn().pipe(
+      map(loggedIn => {
+        if(loggedIn){
+          this.router.navigate(['/','user']);
+          return false;
+        }else{
+          return true;
+        }
+      })
+    );
   }
-  
 }
