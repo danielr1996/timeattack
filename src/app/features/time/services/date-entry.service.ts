6c92ced512016faa4f50cbdcf27f4a0e21fd5b5c
Daniel Richter <daniel.richter@teambank.de>
2020-03-16 14:47:22 +0100

    feat: sync with firebase

--- src/app/features/time/services/date-entry.service.ts
+++ src/app/features/time/services/date-entry.service.ts
@@ -2,15 +2,22 @@ import {Injectable} from '@angular/core';
 import {DateEntryStore} from "../store/date-entry/date-entry.store";
 import {DateEntryQuery} from "../store/date-entry/date-entry.query";
 import {DateEntry} from "../store/date-entry/date-entry";
-import {Observable, of} from "rxjs";
-import {map} from "rxjs/operators";
+import {from, Observable, of} from "rxjs";
+import {map, mergeMap, pluck, tap} from "rxjs/operators";
+import {User} from "firebase";
+import {unpackMaybe} from "src/app/lib/fp/rxjs/unpackMaybe";
+import {AngularFirestore} from "@angular/fire/firestore";
+import {AuthenticationService} from "src/app/features/user/services/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DateEntryService {
+  private user$: Observable<User> = this.authService.getUser().pipe(unpackMaybe());
 
   constructor(
+    private fb: AngularFirestore,
+    private authService: AuthenticationService,
     private dateEntryStore: DateEntryStore,
     private dateEntryQuery: DateEntryQuery
   ) {
@@ -18,7 +25,30 @@ export class DateEntryService {
 
   public add(dateEntry: DateEntry): Observable<void> {
     this.dateEntryStore.add(dateEntry);
-    return of();
+    return this.user$.pipe(
+      mergeMap(user => from(
+        this.fb.collection('data')
+          .doc(user.uid)
+          .collection('dateentries')
+          .doc(dateEntry.id)
+          .set(dateEntry))
+      )
+    );
+  }
+
+  load(): Observable<any> {
+    return this.user$.pipe(
+      mergeMap(user =>
+        this.fb.collection('data')
+          .doc(user.uid)
+          .collection('dateentries')
+          .get()
+      ),
+      pluck('docs'),
+      mergeMap(from),
+      map(data => data.data()),
+      tap(dateEntry => this.dateEntryStore.add(dateEntry)),
+    );
   }
 
   public get(): Observable<DateEntry[]> {
